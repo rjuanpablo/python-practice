14ce7aeeffccd2720022fd2af440eff64e9f9d79
Juan Pablo <58188157+rjuanpablo@users.noreply.github.com>
2024-06-19 20:13:49 -0300

    p01e020304 solved

--- practico_01/ejercicio_04.py
+++ practico_01/ejercicio_04.py
@@ -8,6 +8,20 @@ def es_vocal_if(letra: str) -> bool:
     Restricción: Utilizar un if para cada posibilidad con la función lower().
     Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
     """
+
+    if letra.lower() == 'a':
+        return True
+    elif letra.lower() == 'e':
+        return True
+    elif letra.lower() == 'i':
+        return True
+    elif letra.lower() == 'o':
+        return True
+    elif letra.lower() == 'u':
+        return True
+    else:
+        return False
+
     pass # Completar
 
 
@@ -25,6 +39,10 @@ def es_vocal_if_in(letra: str) -> bool:
     """Re-escribir utilizando un sólo IF y el operador IN.
     Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations
     """
+
+    if letra in "aAeEiIoOuU":
+        return True
+
     pass # Completar
 
 
@@ -40,6 +58,9 @@ assert es_vocal_if_in("A")
 
 def es_vocal_in(letra: str) -> bool:
     """Re-escribir utilizando el operador IN pero sin utilizar IF."""
+
+    return letra in "aAeEiIoOuU"
+
     pass # Completar
 
 
