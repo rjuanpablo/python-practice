c8895e2695ea4e1129a45352a52259f1ae4549d4
Juan Pablo <58188157+rjuanpablo@users.noreply.github.com>
2024-06-20 00:50:01 -0300

    p01e05 solved. p01e06 pending

--- practico_01/ejercicio_06.py
+++ practico_01/ejercicio_06.py
@@ -7,6 +7,18 @@ def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float,
     """Toma una lista de enteros y strings y devuelve una lista con todos los
     elementos numéricos al final.
     """
+
+    intList = []
+    charList = []
+    finalList = []
+
+    for item in lista:
+        if isinstance(item, int):
+            intList.append(item)
+        else:
+            charList.append(item)
+        finalList = charList + intList
+    return finalList
     pass # Completar
 
 
@@ -20,6 +32,11 @@ assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 
 
 def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:
     """Re-escribir utilizando comprensión de listas."""
+
+    charList = [item for item in lista if isinstance(item, str)]
+    intList = [item for item in lista if isinstance(item, (int, float))]
+    return charList + intList
+
     pass # Completar
 
 
@@ -35,6 +52,16 @@ def numeros_al_final_sorted(lista: List[Union[float, str]]) -> List[Union[float,
     """Re-escribir utilizando la función sorted con una custom key.
     Referencia: https://docs.python.org/3/library/functions.html#sorted
     """
+
+
+    #print( sorted(lista, key=lambda x: (not isinstance(x, (int, float)), str(x))))
+    #return listaa
+
+    #listaa = sorted(lista, key=lambda x: (isinstance(x, int), x))
+    #print(listaa)
+
+    #return sorted(lista, key=lambda x: (isinstance(x, str), x))
+
     pass # Completar
 
 
